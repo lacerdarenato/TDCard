918f9e72c3af1e3526f1a53d549b092c7cf529e8
Renato <lacerdarenato00@gmail.com>
2021-06-18 15:47:41 -0300

    Create app Core, routes, views and create serializers for tasks

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import TaskViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
