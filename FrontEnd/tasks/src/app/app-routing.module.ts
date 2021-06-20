caf524453cf4e8fb67eae29861fa2bf628ce21e7
Renato <lacerdarenato00@gmail.com>
2021-06-20 15:06:37 -0300

    Test Route do component Tasks

--- FrontEnd/tasks/src/app/app-routing.module.ts
+++ FrontEnd/tasks/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+
+  {path:'task', component:TasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
